57fb34eea6b6bb01db4b701a1eca0ed329536f05
Sunil kannaujiya <skannaujiya920@gmail.com>
2025-07-25 17:45:11 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    console.log(" MongoDB connected");
   } catch (error) {
     console.error(" MongoDB connection error:", error.message);
     process.exit(1);
